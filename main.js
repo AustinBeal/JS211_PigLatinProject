c4a9d0fa6be84c69f37e1b6a0d07d8f3133bcdec
AustinBeal <austinbeal89@gmail.com>
2020-10-04 14:25:58 -0500

    remade repo due to npm tests not working on other repo, have copied solution to piglatin. passed all tests.

--- main.js
+++ main.js
@@ -11,11 +11,66 @@ const rl = readline.createInterface({
 });
 
 
-const pigLatin = (word) => {
 
-  // Your code here
+  const pigLatin = (word) => {
+      word = word.toLowerCase()
+      word = word.trim()
+      let splitUp = word.split('');
+      //console.log(splitUp)
+      
+    var vowels = ["a", "e", "i", "o", "u"]
+    
+    for (let i = 0; i <= splitUp.length; i++){
+      var currentLetter = splitUp[i]
+      
+    if (vowels.includes(currentLetter)){
+      //console.log('our vowel is', currentLetter ,'at position', [i])
+      let tempV = [i]
+      if (i <= 0){
+  
+        splitUp.push('yay')
+        //console.log(splitUp)
+        
+        let newString2 = splitUp.toString()
+        for(let w = 0; w <= splitUp.length; w++){
+          newString2 = newString2.replace(',', '')
+        }
+        
+        return newString2
 
-}
+        break
+       
+        } else {
+          for(let n = 0; n <= tempV - 1; n++){
+          splitUp.push(splitUp[0])
+          splitUp.splice(0,1)
+        //console.log('our word is ', word ,' and our array is',splitUp)
+        }
+  
+        splitUp.push('ay')
+        let newString = splitUp.toString()
+        
+        for(let j = 0; j <= splitUp.length; j++){
+          newString = newString.replace(',', '')
+          
+        }
+        
+        console.log(newString)
+        return newString
+
+        break
+      
+        }
+        
+      }
+       
+    
+    }
+   
+  }
+
+
+
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
